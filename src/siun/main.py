f6e074ad62b07244a4a79c2a9ef2452ecb7e3112
Thomas <t4k1t+github@protonmail.com>
2023-11-01 15:49:17 +0100

    Add support for config file
    Signed-off-by: Thomas <t4k1t+github@protonmail.com>

--- src/siun/main.py
+++ src/siun/main.py
@@ -2,6 +2,7 @@
 
 """siun - check for pacman updates."""
 
+import collections.abc
 import datetime
 import json
 import re
@@ -14,10 +15,9 @@ from pathlib import Path
 from typing import Optional
 
 import click
+import tomllib
 
 CMD_AVAILABLE = "pacman -Quq"
-CMD_SYNC = "pacman -Sy"
-CRITICAL_PATTERN = r"^archlinux-keyring$|^linux$|^firefox$|^pacman.*$"
 
 
 class StateText(Enum):
@@ -32,6 +32,13 @@ class StateColor(Enum):
     AVAILABLE_UPDATES = "blue"
     WARNING_UPDATES = "yellow"
     CRITICAL_UPDATES = "red"
+
+
+class OutputFormat(Enum):
+    PLAIN = "plain"
+    FANCY = "fancy"
+    JSON = "json"
+    I3STATUS = "i3status"
 
 
 @dataclass
@@ -75,13 +82,12 @@ class StateDecoder(json.JSONDecoder):
 
 
 class Updates:
-    __slots__ = ("state", "thresholds", "count_threshold", "critical_pattern")
+    __slots__ = ("state", "thresholds", "criteria_settings")
 
-    def __init__(self, thresholds: dict):
-        self.count_threshold = 10
-        self.critical_pattern = CRITICAL_PATTERN
+    def __init__(self, thresholds: dict, criteria_settings: dict):
         self.thresholds = thresholds
         self.state = UpdateState(available_updates=[])
+        self.criteria_settings = criteria_settings
 
     def update(self, available_updates: Optional[list] = None):
         self.state.score = 0
@@ -90,8 +96,10 @@ class Updates:
         self.state.available_updates = available_updates
         self.state.count = len(available_updates)
 
+        # Are there any updates?
         if self.state.count > 0:
             self.state.score += 1
+        # Rest of criteria
         if self._criteria_count():
             self.state.score += 1
         if self._criteria_critical():
@@ -107,34 +115,17 @@ class Updates:
         self.state.last_update = datetime.datetime.now(tz=datetime.timezone.utc)
 
     def _criteria_count(self):
-        return len(self.state.available_updates) > self.count_threshold
+        return len(self.state.available_updates) > self.criteria_settings["count_threshold"]
 
     def _criteria_critical(self):
-        regex = re.compile(self.critical_pattern)
+        regex = re.compile(self.criteria_settings["critical_pattern"])
         matches = list(filter(regex.match, self.state.available_updates))
 
         return bool(matches)
 
-    def _criteria_last_update(self):
-        # XXX: Not available through CLI, either cache or try to figure out
-        # through file system
-        raise NotImplementedError
-
-    def _criteria_download_size(self):
-        # XXX: Only available via `-Syu` it seems
-        raise NotImplementedError
-
-
-def _sync_packages():
-    try:
-        subprocess.run(["pacman", "-Sy"], check=True, capture_output=True, text=True)  # noqa
-    except subprocess.CalledProcessError as error:
-        click.echo(f"Failed to sync packages: {error.stderr}", err=True, nl=False)
-        sys.exit(1)
-
 
 def _get_available_updates():
-    cmd = ["pacman", "-Qu"]
+    cmd = ["pacman", "-Quq"]
     if CMD_AVAILABLE:
         cmd = CMD_AVAILABLE.split(" ")
     try:
@@ -167,44 +158,78 @@ def _read_state():
         return json.load(update_file, cls=StateDecoder)
 
 
-def format_plain(state: UpdateState):
-    return state.text_value.value, {}
+class Formatter:
+    @staticmethod
+    def format_plain(state: UpdateState):
+        return state.text_value.value, {}
+
+    @staticmethod
+    def format_fancy(state: UpdateState):
+        return state.text_value.value, {"fg": state.color.value}
+
+    @staticmethod
+    def format_json(state: UpdateState):
+        state_dict = {"count": state.count, "text_value": state.text_value.value, "score": state.score}
+        return json.dumps(state_dict), {}
+
+    @staticmethod
+    def format_i3status(state: UpdateState):
+        i3status_state_map = {
+            "OK": "Idle",
+            "AVAILABLE_UPDATES": "Idle",
+            "WARNING_UPDATES": "Warning",
+            "CRITICAL_UPDATES": "Critical",
+        }
+        i3status_text_map = {
+            "OK": "",
+            "AVAILABLE_UPDATES": "",
+            "WARNING_UPDATES": "Updates recommended",
+            "CRITICAL_UPDATES": "Updates required",
+        }
+        i3status_data = {
+            "icon": "archive",
+            "state": i3status_state_map[state.text_value.name],
+            "text": i3status_text_map[state.text_value.name],
+        }
+        return json.dumps(i3status_data), {}
 
 
-def format_fancy(state: UpdateState):
-    return state.text_value.value, {"fg": state.color.value}
+def load_config():
+    # TODO: error handling
+    with open(Path().home() / ".config" / "siun.toml", "rb") as file_obj:
+        return tomllib.load(file_obj)
 
 
-def format_json(state: UpdateState):
-    state_dict = {"count": state.count, "text_value": state.text_value.value, "score": state.score}
-    return json.dumps(state_dict), {}
-
-
-def format_i3status(state: UpdateState):
-    i3status_state_map = {
-        "OK": "Idle",
-        "AVAILABLE_UPDATES": "Idle",
-        "WARNING_UPDATES": "Warning",
-        "CRITICAL_UPDATES": "Critical",
-    }
-    i3status_text_map = {
-        "OK": "",
-        "AVAILABLE_UPDATES": "",
-        "WARNING_UPDATES": "Updates recommended",
-        "CRITICAL_UPDATES": "Updates required",
-    }
-    i3status_data = {
-        "icon": "archive",
-        "state": i3status_state_map[state.text_value.name],
-        "text": i3status_text_map[state.text_value.name],
-    }
-    return json.dumps(i3status_data), {}
+def update_nested(d, u):
+    # Preserve existing keys of nested dicts
+    # https://stackoverflow.com/questions/3232943/update-value-of-a-nested-dictionary-of-varying-depth
+    for k, v in u.items():
+        if isinstance(v, collections.abc.Mapping):
+            d[k] = update_nested(d.get(k, {}), v)
+        else:
+            d[k] = v
+    return d
 
 
 def main(*, output_format: str):
     """Main function."""
+    config = {
+        "cmd_available": "pacman -Quq",
+        "criteria": {
+            "critical_pattern": "^archlinux-keyring$|^linux$|^firefox$|^pacman.*$",
+            "critical_weight": 1,
+            "count_threshold": 15,
+            "count_weight": 1,
+            "lastupdate_age_seconds": 604800,  # 7 days
+            "lastupdate_weight": 1,
+        }
+    }
+    user_config = load_config()
+    config = update_nested(config, user_config)
+
     thresholds = {0: "OK", 1: "AVAILABLE_UPDATES", 2: "WARNING_UPDATES", 3: "CRITICAL_UPDATES"}
-    updates = Updates(thresholds=thresholds)
+
+    updates = Updates(thresholds=thresholds, criteria_settings=config["criteria"])
     existing_state = _read_state()
     now = datetime.datetime.now(tz=datetime.timezone.utc)
     if existing_state and (existing_state["last_update"] > (now - datetime.timedelta(minutes=30))):
@@ -215,20 +240,14 @@ def main(*, output_format: str):
 
     output = ""
     output_kwargs = {}
-    if output_format == "plain":
-        output, output_kwargs = format_plain(updates.state)
-    elif output_format == "fancy":
-        output, output_kwargs = format_fancy(updates.state)
-    elif output_format == "json":
-        output, output_kwargs = format_json(updates.state)
-    elif output_format == "i3status":
-        output, output_kwargs = format_i3status(updates.state)
+    formatter = Formatter()
+    output, output_kwargs = getattr(formatter, f"format_{output_format}")(updates.state)
     click.secho(output, **output_kwargs)
 
 
 @click.command()
 @click.option(
-    "--output-format", default="plain", type=click.Choice(["plain", "fancy", "json", "i3status"], case_sensitive=False)
+    "--output-format", default="plain", type=click.Choice([of.value for of in OutputFormat], case_sensitive=False)
 )
 def cli(output_format):
     main(output_format=output_format)
