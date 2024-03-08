8237ff4deb273a1f0d2c8a0686b1a660d6cd453f
Thomas <t4k1t+github@protonmail.com>
2024-03-08 10:26:56 +0100

    Update tests
    Signed-off-by: Thomas <t4k1t+github@protonmail.com>

--- tests/test_state.py
+++ tests/test_state.py
@@ -1,5 +1,6 @@
 import pytest
 
+from siun.main import SiunConfig, Threshold
 from siun.state import StateText, Updates
 
 
@@ -8,7 +9,7 @@ class TestUpdates:
 
     def test_defaults_ok(self, default_config, default_thresholds):
         """Test no available updates."""
-        updates = Updates(thresholds_settings=default_thresholds, criteria_settings=default_config["criteria"])
+        updates = Updates(thresholds_settings=default_thresholds, criteria_settings=default_config.criteria)
         updates.update(available_updates=[])
         result = updates.text_value
 
@@ -16,7 +17,7 @@ class TestUpdates:
 
     def test_defaults_available(self, default_config, default_thresholds):
         """Test available updates."""
-        updates = Updates(thresholds_settings=default_thresholds, criteria_settings=default_config["criteria"])
+        updates = Updates(thresholds_settings=default_thresholds, criteria_settings=default_config.criteria)
         updates.update(available_updates=["siun"])
         result = updates.text_value
 
@@ -24,7 +25,7 @@ class TestUpdates:
 
     def test_defaults_recommended(self, default_config, default_thresholds):
         """Test recommended updates."""
-        updates = Updates(thresholds_settings=default_thresholds, criteria_settings=default_config["criteria"])
+        updates = Updates(thresholds_settings=default_thresholds, criteria_settings=default_config.criteria)
         updates.update(available_updates=["siun", "linux"])
         result = updates.text_value
 
@@ -32,7 +33,7 @@ class TestUpdates:
 
     def test_defaults_required(self, default_config, default_thresholds):
         """Test required updates."""
-        updates = Updates(thresholds_settings=default_thresholds, criteria_settings=default_config["criteria"])
+        updates = Updates(thresholds_settings=default_thresholds, criteria_settings=default_config.criteria)
         updates.update(available_updates=["siun", "linux", *["package"] * 15])
         result = updates.text_value
 
@@ -40,23 +41,26 @@ class TestUpdates:
 
 
 @pytest.fixture
-def default_config():
-    """Provide default config."""
-    return {
-        "cmd_available": "pacman -Quq",
-        "criteria": {
-            "available_weight": 1,
-            "critical_pattern": "^archlinux-keyring$|^linux$|^firefox$|^pacman.*$",
-            "critical_weight": 1,
-            "count_threshold": 15,
-            "count_weight": 1,
-            "lastupdate_age_hours": 618,  # 7 days
-            "lastupdate_weight": 1,
-        },
-    }
+def default_thresholds():
+    """Provide default thresholds."""
+    return {Threshold.available: 1, Threshold.warning: 2, Threshold.critical: 3}
 
 
 @pytest.fixture
-def default_thresholds():
-    """Provide default thresholds."""
-    return {"available": 1, "warning": 2, "critical": 3}
+def default_config(default_thresholds):
+    """Provide default config."""
+    return SiunConfig(
+        **{
+            "cmd_available": "pacman -Quq",
+            "thresholds": default_thresholds,
+            "criteria": {
+                "available_weight": 1,
+                "critical_pattern": "^archlinux-keyring$|^linux$|^firefox$|^pacman.*$",
+                "critical_weight": 1,
+                "count_threshold": 15,
+                "count_weight": 1,
+                "lastupdate_age_hours": 618,  # 7 days
+                "lastupdate_weight": 1,
+            },
+        }
+    )
