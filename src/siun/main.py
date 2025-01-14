0ae58f4f1df1af52a7427ac746bf9c3e1c7a82e9
Thomas <t4k1t+github@protonmail.com>
2025-01-14 21:39:43 +0100

    Debug

--- src/siun/main.py
+++ src/siun/main.py
@@ -121,7 +121,6 @@ def check(*, output_format: str, cache: bool, no_update: bool, quiet: bool):
     except ConfigError as error:
         message = f"{error.message}; config path: {error.config_path}"
         raise SiunCLIError(message) from error
-    print(config)
 
     cmd_available = config.cmd_available
     try:
