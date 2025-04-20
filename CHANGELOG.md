da21f49fa635a2605f62e44f69081ee09152c523
Thomas <t4k1t+github@protonmail.com>
2025-04-20 13:04:29 +0200

    Refactor internal state:
    - Removed custom JSON encoder/decoder
    - Merged state methods to Pydantic model
    - Switched to Pydantic for JSON de-/serialization
    
    Signed-off-by: Thomas <t4k1t+github@protonmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,14 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Changed
+
+- Simplified state cache file format. No more custom JSON encoder/decoder required. ⚙️
+
+### Removed
+
+- Custom JSON encoder/decoder.
+
 ## [1.3.0] - 2025-04-16
 
 ### Added
--- tests/test_main.py
+++ tests/test_main.py
@@ -11,9 +11,9 @@ from click.testing import CliRunner
 
 from siun.errors import CmdRunError, ConfigError
 from siun.main import _get_available_updates, _get_updates, check
-from siun.state import SiunState, State
+from siun.state import State, Updates
 
-EMPTY_STATE = SiunState(
+EMPTY_STATE = Updates(
     criteria_settings={},
     thresholds={},
     available_updates=[],
@@ -22,21 +22,7 @@ EMPTY_STATE = SiunState(
     last_update=datetime.datetime.now(tz=datetime.UTC),
 )
 
-STALE_STATE = SiunState(
-    criteria_settings={
-        "available_weight": 1,
-        "critical_weight": 0,
-        "count_weight": 0,
-        "lastupdate_weight": 0,
-    },
-    thresholds={},
-    available_updates=[],
-    matched_criteria={"available": {"weight": 1}},
-    state=State.AVAILABLE_UPDATES,
-    last_update=datetime.datetime.now(tz=datetime.UTC) - datetime.timedelta(days=1),
-)
-
-WARNING_STATE = SiunState(
+WARNING_STATE = Updates(
     criteria_settings={
         "available_weight": 1,
         "critical_weight": 0,
@@ -51,7 +37,7 @@ WARNING_STATE = SiunState(
     last_state=State.AVAILABLE_UPDATES,
 )
 
-AVAILABLE_STATE = SiunState(
+AVAILABLE_STATE = Updates(
     criteria_settings={
         "available_weight": 1,
         "critical_weight": 1,
@@ -75,7 +61,7 @@ class TestMain:
     """Test main function."""
 
     @mock.patch("siun.main.Updates.persist_state")
-    @mock.patch("siun.main.Updates.read_state")
+    @mock.patch("siun.main.load_state")
     @mock.patch("siun.main._get_available_updates", return_value=[])
     @mock.patch("siun.main.get_config")
     def test_check_no_available_updates_no_cache(
@@ -92,7 +78,7 @@ class TestMain:
         assert result.output == "Ok\n"
 
     @mock.patch("siun.main.Updates.persist_state")
-    @mock.patch("siun.main.Updates.read_state")
+    @mock.patch("siun.main.load_state")
     @mock.patch("siun.main._get_available_updates", return_value=[])
     @mock.patch("siun.main.get_config")
     def test_check_no_cache_no_update_options(
@@ -109,7 +95,7 @@ class TestMain:
         assert result.output == "Error: --no-update and --no-cache options are mutually exclusive\n"
 
     @mock.patch("siun.main.Updates.persist_state")
-    @mock.patch("siun.main.Updates.read_state")
+    @mock.patch("siun.main.load_state")
     @mock.patch("siun.main._get_available_updates", return_value=[])
     @mock.patch("siun.main.get_config")
     def test_check_quiet_option(
@@ -126,13 +112,14 @@ class TestMain:
         assert result.output == ""
 
     @mock.patch("siun.main.Updates.persist_state")
-    @mock.patch("siun.main.Updates.read_state", return_value=EMPTY_STATE)
+    @mock.patch("siun.main.load_state")
     @mock.patch("siun.main._get_available_updates", return_value=[])
     @mock.patch("siun.main.get_config")
     def test_check_no_updates(
         self, mock_get_config, mock_get_available_updates, mock_read_state, mock_persist_state, default_config
     ):
         """Test check CLI command with no updates."""
+        mock_read_state.return_value = EMPTY_STATE
         mock_get_config.return_value = default_config
         runner = CliRunner()
         result = runner.invoke(check)
@@ -143,7 +130,7 @@ class TestMain:
         assert result.output == "Ok\n"
 
     @mock.patch("siun.main.Updates.persist_state")
-    @mock.patch("siun.main.Updates.read_state")
+    @mock.patch("siun.main.load_state")
     @mock.patch("siun.main._get_available_updates")
     @mock.patch("siun.main.get_config", side_effect=ConfigError("failed", config_path=Path("/path/to/siun.toml")))
     def test_check_invalid_config(
@@ -160,13 +147,20 @@ class TestMain:
         assert result.output == "Error: failed; config path: /path/to/siun.toml\n"
 
     @mock.patch("siun.main.Updates.persist_state")
-    @mock.patch("siun.main.Updates.read_state", return_value=STALE_STATE)
+    @mock.patch("siun.main.load_state")
     @mock.patch("siun.main._get_available_updates", return_value=["package"])
     @mock.patch("siun.main.get_config")
     def test_check_stale_state(
-        self, mock_get_config, mock_get_available_updates, mock_read_state, mock_persist_state, default_config
+        self,
+        mock_get_config,
+        mock_get_available_updates,
+        mock_read_state,
+        mock_persist_state,
+        default_config,
+        state_stale,
     ):
         """Test check CLI command with stale state on disk."""
+        mock_read_state.return_value = state_stale
         mock_get_config.return_value = default_config
         runner = CliRunner()
         result = runner.invoke(check)
@@ -177,13 +171,20 @@ class TestMain:
         assert result.output == "Updates available\n"
 
     @mock.patch("siun.main.Updates.persist_state")
-    @mock.patch("siun.main.Updates.read_state", return_value=STALE_STATE)
+    @mock.patch("siun.main.load_state")
     @mock.patch("siun.main._get_available_updates", side_effect=CmdRunError("Fuuu"))
     @mock.patch("siun.main.get_config")
     def test_check_with_error_on_get_available_updates(
-        self, mock_get_config, mock_get_available_updates, mock_read_state, mock_persist_state, default_config
+        self,
+        mock_get_config,
+        mock_get_available_updates,
+        mock_read_state,
+        mock_persist_state,
+        default_config,
+        state_stale,
     ):
         """Test check CLI command failing to get available updates."""
+        mock_read_state.return_value = state_stale
         mock_get_config.return_value = default_config
         runner = CliRunner(mix_stderr=False)
         result = runner.invoke(check, ["-n"])
@@ -195,13 +196,20 @@ class TestMain:
         assert result.stderr == "Error: failed to query available updates: Fuuu\n"
 
     @mock.patch("siun.main.Updates.persist_state")
-    @mock.patch("siun.main.Updates.read_state", return_value=STALE_STATE)
+    @mock.patch("siun.main.load_state")
     @mock.patch("siun.main._get_available_updates", return_value=["package"])
     @mock.patch("siun.main.get_config")
     def test_check_with_custom_output_format(
-        self, mock_get_config, mock_get_available_updates, mock_read_state, mock_persist_state, default_config
+        self,
+        mock_get_config,
+        mock_get_available_updates,
+        mock_read_state,
+        mock_persist_state,
+        default_config,
+        state_stale,
     ):
         """Test check CLI command with custom output format."""
+        mock_read_state.return_value = state_stale
         mock_get_config.return_value = default_config
         runner = CliRunner()
         result = runner.invoke(check, ["-o", "custom"])
@@ -212,13 +220,14 @@ class TestMain:
         assert result.output == "Updates available: package\n"
 
     @mock.patch("siun.main.Updates.persist_state")
-    @mock.patch("siun.main.Updates.read_state", return_value=False)
+    @mock.patch("siun.main.load_state", return_value=False)
     @mock.patch("siun.main._get_available_updates", return_value=["package"])
     @mock.patch("siun.config._read_config", return_value=tomllib.loads(CONFIG_CUSTOM_STATE_FILE_PATH))
     def test_custom_state_file_path_config(
         self, mock_read_config, mock_get_available_updates, mock_read_state, mock_persist_state
     ):
         """Test check CLI command with custom state file path."""
+        mock_read_state.return_value = False
         runner = CliRunner()
         with mock.patch(
             "siun.config.CONFIG_PATH"
@@ -257,7 +266,7 @@ class TestMain:
             _get_available_updates(":")
 
     @mock.patch("siun.main.Updates.persist_state")
-    @mock.patch("siun.main.Updates.read_state", return_value=False)
+    @mock.patch("siun.main.load_state", return_value=False)
     @mock.patch("siun.main._update_state", return_value=False)
     def test__get_updates_without_cache_or_update(self, mock__update_state, mock_read_state, mock_persist_state):
         """
@@ -266,6 +275,7 @@ class TestMain:
         The CLI should already catch that no_cache and no_update are mutually
         exclusive, but the code shouldn't fail anyway.
         """
+        mock_read_state.return_value = False
         result = _get_updates(
             no_cache=True,
             no_update=True,
@@ -281,20 +291,20 @@ class TestMain:
         assert result.score == 0
 
     @mock.patch("siun.main.Updates.persist_state")
-    @mock.patch("siun.main.Updates.read_state")
+    @mock.patch("siun.main.load_state")
     @mock.patch("siun.main._update_state", return_value=False)
     def test__get_updates_with_no_update_and_existing_cache(
         self, mock__update_state, mock_read_state, mock_persist_state
     ):
         """Test _get_updates with no_update."""
-        existing_state = {
-            "last_update": {"py-type": "datetime", "value": "1970-01-01T01:00:00Z"},
-            "state": {"py-type": "State", "value": "OK"},
-            "thresholds": {},
-            "matched_criteria": {},
-            "available_updates": ["siun"],
-            "criteria_settings": {},
-        }
+        existing_state = Updates(
+            last_update="1970-01-01T01:00:00Z",
+            state="OK",
+            thresholds={},
+            matched_criteria={},
+            available_updates=["siun"],
+            criteria_settings={},
+        )
         mock_read_state.return_value = existing_state
         result = _get_updates(
             no_cache=False,
@@ -324,29 +334,54 @@ class TestMain:
         assert result.exit_code == 0
         assert result.output == "Updates available\n"
 
-    @pytest.mark.feature_notification
-    @mock.patch("siun.main.Updates.set_last_state")  # Don't update `last_state`, it's already set in the fixture
-    @mock.patch("siun.main.Updates.read_state")
+    @mock.patch("siun.main.Updates.persist_state")
+    @mock.patch("siun.main._get_available_updates", return_value=["package"])
+    @mock.patch("siun.main.load_state")
     @mock.patch("siun.notification.UpdateNotification.show")
     @mock.patch("siun.main.get_config")
-    def test_check_notification_threshold(
-        self, mock_get_config, mock_show, mock_read_state, mock_set_last_state, config_w_notification_threshold
+    def test_check_notification_below_threshold(
+        self,
+        mock_get_config,
+        mock_show,
+        mock_read_state,
+        mock_get_updates,
+        mock_persist_state,
+        config_w_notification_threshold,
+        state_stale,
     ):
-        """Test check CLI command with notification threshold set to `warning`."""
+        """Test state below threshold does not show notification."""
         mock_get_config.return_value = config_w_notification_threshold
-
         # `available` state is below notification threshold
-        mock_read_state.return_value = AVAILABLE_STATE
+        mock_read_state.return_value = state_stale
         runner = CliRunner()
-        result = runner.invoke(check, ["-U"])
+        result = runner.invoke(check)
         mock_show.assert_not_called()
         assert result.exit_code == 0
         assert result.output == "Updates available\n"
 
+    @pytest.mark.feature_notification
+    @mock.patch("siun.main.Updates.persist_state")
+    @mock.patch("siun.main._get_available_updates", return_value=["package", "other_package"])
+    @mock.patch("siun.main.load_state")
+    @mock.patch("siun.notification.UpdateNotification.show")
+    @mock.patch("siun.main.get_config")
+    def test_check_notification_above_threshold(
+        self,
+        mock_get_config,
+        mock_show,
+        mock_read_state,
+        mock_get_updates,
+        mock_persist_state,
+        config_w_notification_threshold,
+        state_stale,
+    ):
+        """Test state gte threshold shows notification."""
+        mock_get_config.return_value = config_w_notification_threshold
         # `warning` state exceeds notification threshold
-        mock_read_state.return_value = WARNING_STATE
+        mock_read_state.return_value = state_stale
         runner = CliRunner()
-        result = runner.invoke(check, ["-U"])
+        result = runner.invoke(check)
+        assert result.output == "Updates recommended\n"
         mock_show.assert_called_once()
         assert result.exit_code == 0
         assert result.output == "Updates recommended\n"
